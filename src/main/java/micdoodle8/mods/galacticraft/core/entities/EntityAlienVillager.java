302a6992957b595271633011035c2021faa1a0e8
nictrace <bardclub@gmail.com>
2019-09-18 07:06:45 +0300

    add interact() to moon villager

--- src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityAlienVillager.java
+++ src/main/java/micdoodle8/mods/galacticraft/core/entities/EntityAlienVillager.java
@@ -12,9 +12,7 @@ import micdoodle8.mods.galacticraft.core.items.GCItems;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.IMob;
-import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -59,7 +57,7 @@ public class EntityAlienVillager extends EntityAgeable implements IEntityBreatha
         this.tasks.addTask(4, new EntityAIOpenDoor(this, true));
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 0.3F));
         this.tasks.addTask(9, new EntityAIWatchClosest2(this, EntityPlayer.class, 15.0F, 1.0F));
-        this.tasks.addTask(9, new EntityAIWatchClosest2(this, EntityVillager.class, 15.0F, 0.05F));
+        this.tasks.addTask(9, new EntityAIWatchClosest2(this, EntityAlienVillager.class, 15.0F, 0.05F));
         this.tasks.addTask(9, new EntityAIWander(this, 0.3F));
         this.tasks.addTask(10, new EntityAIWatchClosest(this, EntityLiving.class, 15.0F));
     }
@@ -111,7 +109,29 @@ public class EntityAlienVillager extends EntityAgeable implements IEntityBreatha
 
         super.updateAITick();
     }
+    /**
+     * Called when a player interacts with a mob. e.g. gets milk from a cow, gets into the saddle on a pig.
+     */
+    public boolean interact(EntityPlayer p_70085_1_)
+    {
+        ItemStack itemstack = p_70085_1_.inventory.getCurrentItem();
+        boolean flag = itemstack != null && itemstack.getItem() == Items.spawn_egg;
 
+        if (!flag && this.isEntityAlive() && !this.isTrading() && !this.isChild() && !p_70085_1_.isSneaking())
+        {
+            if (!this.worldObj.isRemote)
+            {
+                this.setCustomer(p_70085_1_);
+                p_70085_1_.displayGUIMerchant(this, this.getCustomNameTag());
+            }
+
+            return true;
+        }
+        else
+        {
+            return super.interact(p_70085_1_);
+        }
+    }
     @Override
     protected void entityInit()
     {
